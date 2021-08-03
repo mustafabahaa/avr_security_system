b1958d2de4c9eb1642186cbdcb4d0fb5a76136b8
Mustafa <mustafa.bahaa123@gmail.com>
2021-08-03 21:34:34 +0200

    remove debug folder files because it's uploaded before

--- 2_sourceCode/HMI_ECU/Door Locker - HMI_ECU/Debug/STACK/Utility/std_library/std_library.d
+++
@@ -1,9 +0,0 @@
-STACK/Utility/std_library/std_library.d \
- STACK/Utility/std_library/std_library.o: \
- ../../../../2_sourceCode/STACK/Utility/std_library/std_library.c \
- ../../../../2_sourceCode/STACK/Utility/std_library/std_library.h \
- c:\users\mbahaa\desktop\atmega32\2_sourcecode\stack\bsp\includes\types.h
-
-../../../../2_sourceCode/STACK/Utility/std_library/std_library.h:
-
-c:\users\mbahaa\desktop\atmega32\2_sourcecode\stack\bsp\includes\types.h:
