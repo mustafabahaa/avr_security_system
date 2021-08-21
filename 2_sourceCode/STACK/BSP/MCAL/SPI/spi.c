97e6bce4f5d4f7966518bf6bc55079d5e32bdd89
Mustafa <mustafa.bahaa123@gmail.com>
2021-08-22 01:20:03 +0200

    add SPI Driver

---
+++ 2_sourceCode/STACK/BSP/MCAL/SPI/spi.c
@@ -0,0 +1,137 @@
+
+/**************************************************************************
+ ** This  software  is  in  the  public  domain , furnished "as is", without
+ ** technical support,  and with no  warranty, express or implied, as to its
+ ** usefulness for any purpose.
+
+ ** spi.c
+ ***************************************************************************
+ **                              Includes                                 **
+ **************************************************************************/
+#include "spi.h"
+/**************************************************************************/
+/*                     Functions Implementation                           */
+/**************************************************************************/
+spi_error_t mcal_spi_init(spi_t *spi)
+{
+  spi_error_t error = SPI_STATE_SUCCESS;
+
+  switch (spi->mode)
+  {
+  case SPI_MODE_MASTER:
+  {
+
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.mosi_pin, DIR_OUTPUT);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.sci_pin, DIR_OUTPUT);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.ss_pin, DIR_OUTPUT);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.miso_pin, DIR_INPUT_PULLDOWN);
+    reg_write(SPCR, en_bit(SPE) | en_bit(MSTR));
+    break;
+  }
+
+  case SPI_MODE_SLAVE:
+  {
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.mosi_pin, DIR_INPUT_PULLDOWN);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.sci_pin, DIR_INPUT_PULLDOWN);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.ss_pin, DIR_INPUT_PULLDOWN);
+    mcal_gpio_pin_init(spi->pinning.base_addr, spi->pinning.miso_pin, DIR_OUTPUT);
+    reg_write(SPCR, en_bit(SPE));
+    break;
+  }
+
+  default:
+  {
+    error = SPI_STATE_INVALID_MODE;
+    break;
+  }
+  }
+
+  switch (spi->clock)
+  {
+  case SPI_SCK_FREQUANCY_FOC_4:
+  {
+    /* default */
+    break;
+  }
+
+  case SPI_SCK_FREQUANCY_FOC_16:
+  {
+    set_bit(SPCR,SPR0);
+    break;
+  }
+
+  case SPI_SCK_FREQUANCY_FOC_64:
+  {
+    set_bit(SPCR,SPR1);
+    break;
+  }
+
+  case SPI_SCK_FREQUANCY_FOC_128:
+  {
+    set_bit(SPCR,SPR0);
+    set_bit(SPCR,SPR1);
+    break;
+  }
+
+  default:
+  {
+    error = SPI_STATE_INVALID_CLOCK;
+    break;
+  }
+  }
+
+  if (spi->features.double_speed)
+  {
+    set_bit(SPSR, SPI2X);
+  }
+  else
+  {
+    clr_bit(SPSR, SPI2X);
+  }
+
+  return error;
+}
+
+spi_error_t mcal_spi_send_byte(u8_t data)
+{
+  spi_error_t error = SPI_STATE_SUCCESS;
+
+  reg_write(SPDR, data);
+
+  /* check if operation of transmit or receive is running */
+  while (bit_is_clr(SPSR, SPIF))
+    ;
+
+  return error;
+}
+
+spi_error_t mcal_spi_receive_byte(u8_t *data)
+{
+  spi_error_t error = SPI_STATE_SUCCESS;
+
+  /* check if operation of transmit or receive is running */
+  while (bit_is_clr(SPSR, SPIF));
+
+  reg_read(SPDR, *data);
+
+  return error;
+}
+
+spi_error_t mcal_spi_swap_byte(u8_t *data)
+{
+  spi_error_t error = SPI_STATE_SUCCESS;
+
+  /* send 0XFF to salve to shift all the 8 bits in the data buffer
+	    in the slave and receive it at the master , if you don't 
+		understand this please read more on SPI communication protocol */
+  if (SPI_STATE_SUCCESS != mcal_spi_send_byte(DUMB))
+  {
+    error = SPI_STATE_ERROR;
+  }
+  else
+  {
+    reg_read(SPDR, *data);
+  }
+  /* receive the value you wanted from slave */
+  return error;
+}
---
+++ 2_sourceCode/STACK/BSP/MCAL/SPI/spi.h
@@ -0,0 +1,118 @@
+/**************************************************************************
+ ** This  software  is  in  the  public  domain , furnished "as is", without
+ ** technical support,  and with no  warranty, express or implied, as to its
+ ** usefulness for any purpose.
+
+ ** spi.h
+ **************************************************************************/
+#ifndef SPI_H_
+#define SPI_H_
+/*************************************************************************/
+/*                              Includes                                 */
+/*************************************************************************/
+#include "types.h"
+#include "atmega32.h"
+#include "gpio.h"
+/*************************************************************************/
+/*                              Macros                                   */
+/*************************************************************************/
+#define DUMB 0XFF
+
+/*define port and of SPI control */
+#define DDR_SPI DDRB
+#define DD_SS PB4
+#define DD_MOSI PB5
+#define DD_MISO PB6
+#define DD_SCK PB7
+/*************************************************************************/
+/*                               Types                                   */
+/*************************************************************************/
+typedef enum
+{
+  SPI_STATE_SUCCESS = 1,
+  SPI_STATE_ERROR,
+  SPI_STATE_INVALID_ENDIAN,
+  SPI_STATE_INVALID_MODE,
+  SPI_STATE_INVALID_CLOCK,
+} spi_error_t;
+
+typedef enum
+{
+  SPI_SCK_FREQUANCY_FOC_4 = 1,
+  SPI_SCK_FREQUANCY_FOC_16,
+  SPI_SCK_FREQUANCY_FOC_64,
+  SPI_SCK_FREQUANCY_FOC_128,
+} spi_clock_t;
+
+typedef enum
+{
+  SPI_MODE_MASTER = 1,
+  SPI_MODE_SLAVE,
+} spi_mode_t;
+
+typedef struct
+{
+  u8_t double_speed;
+} spi_features_t;
+
+typedef struct
+{
+  u8_t base_addr;
+  u8_t ss_pin;
+  u8_t mosi_pin;
+  u8_t miso_pin;
+  u8_t sci_pin;
+} spi_pinning_t;
+
+typedef struct
+{
+  spi_pinning_t pinning;
+  spi_mode_t mode;
+  spi_clock_t clock;
+  spi_features_t features;
+} spi_t;
+
+/*************************************************************************/
+/*                           Public Functions                            */
+/**************************************************************************
+ ** mcal_spi_init()
+ **
+ ** parameters: spi_t* spi
+ ** return    : spi_error_t
+ ***************************************************************************
+ ** this function is used to initialize SPI if it is in master mode or slave
+ **************************************************************************/
+spi_error_t mcal_spi_init(spi_t *spi);
+
+/**************************************************************************
+ ** mcal_spi_send_byte()
+ **
+ ** parameters: u8_t data
+ ** return    : spi_error_t
+ ***************************************************************************
+ ** this function is used to send bytes "data"
+ **************************************************************************/
+spi_error_t mcal_spi_send_byte(u8_t data);
+
+/**************************************************************************
+ ** mcal_spi_receive_byte()
+ **
+ ** parameters: u8_t* data
+ ** return    : spi_error_t
+ ***************************************************************************
+ ** this function is used to recieve bytes "data"
+ **************************************************************************/
+spi_error_t mcal_spi_receive_byte(u8_t *data);
+
+/**************************************************************************
+ ** mcal_spi_swap_byte()
+ **
+ ** parameters: u8_t* data
+ ** return    : spi_error_t
+ ***************************************************************************
+** this function returns the 8 bits in the slave buffer by sending 
+** dumb useless 8 bits to the buffer.
+ **************************************************************************/
+spi_error_t mcal_spi_swap_byte(u8_t *data);
+
+#endif /* SPI_H_ */
