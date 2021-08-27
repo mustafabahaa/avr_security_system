c751fad3ef8510f7d97c0a4af5a2ad566016daba
Mustafa <mustafa.bahaa123@gmail.com>
2021-08-27 19:17:13 +0200

    make button component use logger

--- 2_sourceCode/STACK/BSP/HAL/BUTTON/button.c
+++ 2_sourceCode/STACK/BSP/HAL/BUTTON/button.c
@@ -1,4 +1,4 @@
-/**************************************************************************
+/***************************************************************************
  ** This  software  is  in  the  public  domain , furnished "as is", without
  ** technical support,  and with no  warranty, express or implied, as to its
  ** usefulness for any purpose.
@@ -8,79 +8,94 @@
  **                              Includes                                 **
  **************************************************************************/
 #include "button.h"
-
 /*************************************************************************/
-/*                     Functions Implementation                          */
+/*                          Global Variables                             */
 /*************************************************************************/
+static u8_t *TAG = (u8_t *)"BUTTON";
+/**************************************************************************/
+/*                     Functions Implementation                           */
+/**************************************************************************/
 
 button_error_t hal_button_init(button_t *the_button)
 {
-	button_error_t error = BUTTON_GPIO_STATE_SUCCESS;
+  button_error_t error = BUTTON_GPIO_STATE_SUCCESS;
 
-	switch (the_button->connection)
-	{
-	case PULLUP_CONNECTION:
-		if (GPIO_STATE_SUCCESS == mcal_gpio_pin_init(
-				the_button->base_addr,
-				the_button->pin_num, DIR_INPUT_PULLUP))
-		{
-			/* button initialized */
-		}
-		else
-		{
-			error = BUTTON_GPIO_STATE_ERROR;
-		}
-		break;
+  switch (the_button->connection)
+  {
+  case PULLUP_CONNECTION:
+  {
+    if (GPIO_STATE_SUCCESS != mcal_gpio_pin_init(
+                                  the_button->base_addr,
+                                  the_button->pin_num, DIR_INPUT_PULLUP))
+    {
+      error = BUTTON_GPIO_STATE_ERROR;
+      logger_write_error_println(LOG_HAL, TAG, (u8_t *)"initialization fail");
+    }
+    else
+    {
+      logger_write_debug_println(LOG_HAL, TAG, (u8_t *)"initialization success");
+    }
+    break;
+  }
 
-	case PULLDOWN_CONNECTION:
-		if (GPIO_STATE_SUCCESS == mcal_gpio_pin_init(
-				the_button->base_addr,
-				the_button->pin_num, DIR_INPUT_PULLDOWN))
-		{
-			/* button initialized */
-		}
-		else
-		{
-			error = BUTTON_GPIO_STATE_ERROR;
-		}
-		break;
+  case PULLDOWN_CONNECTION:
+  {
+    if (GPIO_STATE_SUCCESS != mcal_gpio_pin_init(
+                                  the_button->base_addr,
+                                  the_button->pin_num, DIR_INPUT_PULLDOWN))
+    {
+      error = BUTTON_GPIO_STATE_ERROR;
+      logger_write_error_println(LOG_HAL, TAG, (u8_t *)"initialization fail");
+    }
+    else
+    {
+      logger_write_debug_println(LOG_HAL, TAG, (u8_t *)"initialization success");
+    }
+    break;
+  }
 
-	default:
-		error = BUTTON_GPIO_INVALID_CONNECTION;
-		break;
-	}
-	return error;
+  default:
+  {
+    error = BUTTON_GPIO_INVALID_CONNECTION;
+    logger_write_error_println(LOG_HAL, TAG, (u8_t *)"invalid connection type");
+
+    break;
+  }
+  }
+  return error;
 }
 
 button_error_t hal_button_get_state(
-		button_t *the_button,
-		button_states_t *result)
+    button_t *the_button,
+    button_states_t *result)
 {
-	button_error_t error = BUTTON_GPIO_STATE_SUCCESS;
+  button_error_t error = BUTTON_GPIO_STATE_SUCCESS;
 
-	button_states_t states;
+  button_states_t states;
 
-	if (GPIO_STATE_SUCCESS == mcal_gpio_pin_read(
-			the_button->base_addr,
-			the_button->pin_num, &states))
-	{
-		if (the_button->connection == PULLDOWN_CONNECTION)
-		{
-			*result = (button_states_t)states;
-		}
-		else if (the_button->connection == PULLUP_CONNECTION)
-		{
-			*result = (button_states_t)(!states);
-		}
-		else
-		{
-			error = BUTTON_GPIO_INVALID_CONNECTION;
-		}
-	}
-	else
-	{
-		error = BUTTON_GPIO_STATE_ERROR;
-	}
+  if (GPIO_STATE_SUCCESS != mcal_gpio_pin_read(
+                                the_button->base_addr,
+                                the_button->pin_num, &states))
+  {
+    error = BUTTON_GPIO_STATE_ERROR;
+    logger_write_error_println(LOG_HAL, TAG, (u8_t *)"get status failure");
+  }
+  else
+  {
+    if (the_button->connection == PULLDOWN_CONNECTION)
+    {
+      *result = (button_states_t)states;
+    }
+    else if (the_button->connection == PULLUP_CONNECTION)
+    {
+      *result = (button_states_t)(!states);
+    }
+    else
+    {
+      error = BUTTON_GPIO_INVALID_CONNECTION;
+      logger_write_error_println(LOG_HAL, TAG, (u8_t *)"invalid connection type");
+    }
+  }
 
-	return error;
+  return error;
 }
--- 2_sourceCode/STACK/BSP/HAL/BUTTON/button.h
+++ 2_sourceCode/STACK/BSP/HAL/BUTTON/button.h
@@ -1,4 +1,4 @@
-/**************************************************************************
+/***************************************************************************
  ** This  software  is  in  the  public  domain , furnished "as is", without
  ** technical support,  and with no  warranty, express or implied, as to its
  ** usefulness for any purpose.
@@ -7,54 +7,56 @@
  **************************************************************************/
 #ifndef _BUTTON_H_
 #define _BUTTON_H_
-/*************************************************************************/
-/*                              Includes                                 */
-/*************************************************************************/
+/**************************************************************************/
+/*                              Includes                                  */
+/**************************************************************************/
 #include "types.h"
 #include "gpio.h"
+#include "logger.h"
 
-/*************************************************************************/
-/*                                 Types                                 */
-/*************************************************************************/
+/**************************************************************************/
+/*                                 Types                                  */
+/**************************************************************************/
 typedef enum
 {
-	PULLUP_CONNECTION,
-	PULLDOWN_CONNECTION
+  PULLUP_CONNECTION = 1,
+  PULLDOWN_CONNECTION
 } buttonConnection_t;
 
 typedef enum
 {
-	BUTTON_GPIO_STATE_SUCCESS,
-	BUTTON_GPIO_INVALID_CONNECTION,
-	BUTTON_GPIO_STATE_ERROR
+  BUTTON_GPIO_STATE_SUCCESS = 1,
+  BUTTON_GPIO_INVALID_CONNECTION,
+  BUTTON_GPIO_STATE_ERROR
 } button_error_t;
 
 typedef enum
 {
-	BUTTON_NOT_PRESSED,
-	BUTTON_PRESSED
+  BUTTON_NOT_PRESSED = 1,
+  BUTTON_PRESSED
 } button_states_t;
 
 typedef struct button
 {
-	u8_t base_addr;
-	u8_t pin_num;
-	buttonConnection_t connection;
+  u8_t base_addr;
+  u8_t pin_num;
+  buttonConnection_t connection;
 } button_t;
 
-/*************************************************************************/
-/*                           Public Functions                            */
-/**************************************************************************
+/**************************************************************************/
+/*                           Public Functions                             */
+/***************************************************************************
  ** hal_button_init()
  **
  ** parameter: button_t *the_button
  ** return    : button_error_t
  ***************************************************************************
- ** this function is used to initialize all the necessary sequence for button
+ ** this function is used to initialize all the necessary
+ ** sequence for button
  **************************************************************************/
 button_error_t hal_button_init(button_t *the_button);
 
-/**************************************************************************
+/***************************************************************************
  ** hal_button_init()
  **
  ** parameter: button_t *the_button
@@ -64,7 +66,6 @@ button_error_t hal_button_init(button_t *the_button);
  ** this function return the states of the button On or off
  **************************************************************************/
 button_error_t hal_button_get_state(button_t *the_button,
-		button_states_t *result);
+                                    button_states_t *result);
 
 #endif /*_BUTTON_H_*/
-/*************************************************************************/
